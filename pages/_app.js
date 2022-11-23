2a1f34373fa848f92ae6f0eb4434aa9dce69f391
JackYouk <jackyoukstetter1@gmail.com>
2022-11-22 23:49:52 -0800

    v1 in the books

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/reset.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    import("bootstrap/dist/js/bootstrap");
+  },[]);
   return <Component {...pageProps} />
 }
 
